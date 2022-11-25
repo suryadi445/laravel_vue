d41a8a81c78232fb595213d4e45ae79a46d85ba7
suryadi445 <suryadi.hhb@gmail.com>
2022-11-25 21:32:34 +0700

    create app.css in public/css and create route /home

---
+++ resources/views/template/vue.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Vue</title>
+
+    <style>
+        body {
+            margin: 15%;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div id="app">
+        <h2>Laravel + Vue JS</h2>
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+
+</html>
