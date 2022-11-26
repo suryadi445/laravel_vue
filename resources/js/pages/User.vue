fd69193af7431b8a482f1c6a36b32128c5494eb1
suryadi445 <suryadi.hhb@gmail.com>
2022-11-26 20:42:41 +0700

    create list user and looping using v-for

--- resources/js/pages/User.vue
+++ resources/js/pages/User.vue
@@ -4,11 +4,34 @@
 </section>
 <section v-else>
     <h1>Daftar List</h1>
+    <ol>
+        <li v-for="user in users" :key="user.id">
+            {{user.name}}
+        </li>
+    </ol>
 </section>
 </template>
 
 <script>
 export default {
-    props: ['nama']
+    props: ['nama'],
+    data () {
+            return {
+               users : [
+                  {
+                      id: 1,
+                      name: 'Suryadi'
+                  },
+                  {
+                      id: 2,
+                      name: 'Khalisa'
+                  },
+                  {
+                      id: 3,
+                      name: 'M. Hafiz'
+                  }
+               ]
+            }
+        }
 }
 </script>
