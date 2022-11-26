3f86573a17e1a39fbd6aa0dd452b693f1a66a474
suryadi445 <suryadi.hhb@gmail.com>
2022-11-26 21:00:17 +0700

    create profile pages and using methods in script for setting url in router link

--- resources/js/pages/User.vue
+++ resources/js/pages/User.vue
@@ -1,37 +1,45 @@
 <template>
-<section v-if="nama">
-<h1>Hello, {{ nama }}</h1>
-</section>
-<section v-else>
+  <section v-if="nama">
+    <h1>Hello, {{ nama }}</h1>
+
+    <router-link :to="{ name: 'User' }">Kembali</router-link>
+  </section>
+
+  <section v-else>
     <h1>Daftar List</h1>
     <ol>
-        <li v-for="user in users" :key="user.id">
-            {{user.name}}
-        </li>
+      <li v-for="user in users" :key="user.id">
+        <router-link :to="profil_name(user.name)">{{ user.name }}</router-link>
+      </li>
     </ol>
-</section>
+  </section>
 </template>
 
 <script>
 export default {
-    props: ['nama'],
-    data () {
-            return {
-               users : [
-                  {
-                      id: 1,
-                      name: 'Suryadi'
-                  },
-                  {
-                      id: 2,
-                      name: 'Khalisa'
-                  },
-                  {
-                      id: 3,
-                      name: 'M. Hafiz'
-                  }
-               ]
-            }
-        }
-}
+  props: ["nama"],
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Suryadi",
+        },
+        {
+          id: 2,
+          name: "Khalisa",
+        },
+        {
+          id: 3,
+          name: "M.Hafiz",
+        },
+      ],
+    };
+  },
+  methods: {
+    profil_name(nama) {
+      return "/user/" + nama.toLowerCase();
+    },
+  },
+};
 </script>
