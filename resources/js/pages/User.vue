c1020165ed522851fbb9ef11d2ee2c5dc71b09d7
suryadi445 <suryadi.hhb@gmail.com>
2022-11-26 22:09:30 +0700

    create router push in user.vue

--- resources/js/pages/User.vue
+++ resources/js/pages/User.vue
@@ -9,7 +9,8 @@
     <h1>Daftar List</h1>
     <ol>
       <li v-for="user in users" :key="user.id">
-        <router-link :to="profil_name(user.name)">{{ user.name }}</router-link>
+        <!-- <router-link :to="profil_name(user.name)">{{ user.name }}</router-link> -->
+        <a href="" @click.prevent="lihatUser(user.name)">{{ user.name }}</a>
       </li>
     </ol>
   </section>
@@ -39,6 +40,9 @@ export default {
   methods: {
     profil_name(nama) {
       return "/user/" + nama.toLowerCase();
+    },
+    lihatUser(nama) {
+      this.$router.push("/user/" + nama.toLowerCase());
     },
   },
 };
