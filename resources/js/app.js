36931c2615afe03cb6d5569cfa857055d41c66f0
suryadi445 <suryadi.hhb@gmail.com>
2022-11-26 18:46:24 +0700

    create error handling 404 not found in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,11 +8,13 @@ import './bootstrap';
 import { createApp } from 'vue';
 import Home from './pages/Home.vue'
 import About from './pages/About.vue'
+import NotFound from './pages/NotFound.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
     { path: '/home', component: Home },
     { path: '/about', component: About },
+    { path: "/:catchAll(.*)", component: NotFound },
 ]
 
 const router = createRouter({
