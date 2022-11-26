6f3978ffce08f95b733c022cd08e2b9d5ad79715
suryadi445 <suryadi.hhb@gmail.com>
2022-11-26 19:01:00 +0700

    create file export router in router/index.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,21 +6,7 @@
 
 import './bootstrap';
 import { createApp } from 'vue';
-import Home from './pages/Home.vue'
-import About from './pages/About.vue'
-import NotFound from './pages/NotFound.vue'
-import { createRouter, createWebHistory } from 'vue-router'
-
-const routes = [
-    { path: '/home', component: Home },
-    { path: '/about', component: About },
-    { path: "/:catchAll(.*)", component: NotFound },
-]
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes, // short for `routes: routes`
-})
+import router from './router';
 
 const app = createApp({
     data() {
