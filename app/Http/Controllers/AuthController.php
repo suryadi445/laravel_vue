86936a07f3701d3bd1a8a78988c30162ecce08ec
suryadi445 <suryadi.hhb@gmail.com>
2022-11-29 08:38:45 +0700

    change query builder in authCOntroller to be eloquent ORM

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+
 
 class AuthController extends Controller
 {
     public function index()
     {
-        $data = DB::table('users')->select('id', 'name')->get();
+        $data = User::all();
 
         return response()->json($data);
     }
 
     public function show($id)
     {
-        $data = DB::table('users')->where('id', $id)->first();
+        $data = User::find($id);
 
         return response()->json($data);
     }
